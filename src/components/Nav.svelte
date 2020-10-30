28d89aeb419c2576cd75c36a89c39f12b065fa0d
Andre Bellmann <andre.bellmann@huk-coburg.de>
2020-10-30 16:09:56 +0100

    nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,10 +51,6 @@
 <nav>
 	<ul>
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'impressum' ? 'page' : undefined}" href="impressum">Impressum</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">404</a></li>
+		<li><a rel=prefetch aria-current="{segment === 'impressum' ? 'page' : undefined}" href="impressum">Impressum</a></li>
 	</ul>
 </nav>
