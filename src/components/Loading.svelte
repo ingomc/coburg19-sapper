edb61e1310fbafe1109b535f69ae2178e4bf448c
Andre Bellmann <andre.bellmann@huk-coburg.de>
2020-11-02 23:52:25 +0100

    loading spinner

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -1,3 +1,30 @@
-<div>
-    Loading
+<script>
+  import { fade } from "svelte/transition";
+</script>
+
+<style>
+  .spinner {
+    position: fixed;
+    top: 1rem;
+    right: 1rem;
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    border: 2px solid rgba(255, 255, 255, 0.4);
+    border-bottom-color: var(--color);
+    animation: rotate 0.3s linear infinite;
+  }
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
+
+<div transition:fade>
+  <div class="spinner" />
 </div>
