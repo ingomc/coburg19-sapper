70e79a30b076de7770f732fee423c1351eb0529c
Andre Bellmann <andre.bellmann@huk-coburg.de>
2020-12-05 15:55:37 +0100

    zeitreise

--- src/components/Timetravel.svelte
+++ src/components/Timetravel.svelte
@@ -1,10 +1,18 @@
 <script>
   import { onMount } from 'svelte';
   import RangeSlider from 'svelte-range-slider-pips';
+  import moment from 'moment';
   import { async_data } from '../stores/stores';
 
-  const moods = ['😁', '😀', '😊', '😐', '😥', '😫'];
-  let values = [moods.length - 1];
+  const interval = ['Vor 1 Monat', 'vor 1 Woche', 'Gestern', 'Heute'];
+  moment.locale();
+  const oldDate = [
+    moment().subtract(1, 'months').format('DD.MM.YYYY'),
+    moment().subtract(1, 'weeks').format('DD.MM.YYYY'),
+    'Gestern',
+    'Heute',
+  ];
+  let values = [interval.length - 1];
   let changedValue = 0;
 
   function fetchData() {
@@ -17,7 +25,7 @@
     changedValue = Math.round(values[0]);
   }
   // just trigger when actual value changes
-  $: if (changedValue >= 0 && changedValue != moods.length - 1) {
+  $: if (changedValue >= 0 && changedValue != interval.length - 1) {
     console.log(changedValue);
     if (process.browser) {
       const newData = fetchData();
@@ -42,10 +50,11 @@
     bind:values
     range="max"
     springValues="{{ stiffness: 0.3, damping: 0.9 }}"
-    formatter="{(v) => moods[v]}"
-    max="{moods.length - 1}"
+    formatter="{(v) => interval[v]}"
+    max="{interval.length - 1}"
   />
-  <p>{moods[values[0]]}</p>
+  <p>{interval[values[0]]}</p>
   <p>{values[0]}</p>
   <p>{changedValue}</p>
+  <p>{oldDate[values[0]]}</p>
 </div>
