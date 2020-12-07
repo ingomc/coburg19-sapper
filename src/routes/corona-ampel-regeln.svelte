cc79b24fec49505e1c59d0c1ca23533068047161
Andre Bellmann <andre.bellmann@huk-coburg.de>
2020-12-07 12:33:12 +0100

    remove wordwrap

--- src/routes/corona-ampel-regeln.svelte
+++ src/routes/corona-ampel-regeln.svelte
@@ -46,7 +46,7 @@
           d="M14.8 4.613l6.701 11.161c.963 1.603.49 3.712-1.057 4.71a3.213 3.213 0 0 1-1.743.516H5.298C3.477 21 2 19.47 2 17.581c0-.639.173-1.264.498-1.807L9.2 4.613c.962-1.603 2.996-2.094 4.543-1.096c.428.276.79.651 1.057 1.096zm-2.22.839a1.077 1.077 0 0 0-1.514.365L4.365 16.98a1.17 1.17 0 0 0-.166.602c0 .63.492 1.14 1.1 1.14H18.7c.206 0 .407-.06.581-.172a1.164 1.164 0 0 0 .353-1.57L12.933 5.817a1.12 1.12 0 0 0-.352-.365zM12 17a1 1 0 1 1 0-2a1 1 0 0 1 0 2zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"
           fill="currentColor"
         ></path></svg>
-      <span> Aktuell sind die Maßnahmen der Corona-Ampel irrelevant! </span>
+      <span> Aktuell gelten zusätzliche Verschärfungen! </span>
     </small>
   </Message>
   <h1>Was bedeuten die Farben der Corona-Ampel?</h1>
