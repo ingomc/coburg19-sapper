d88c8e06f66347f6267ae0bad0e01a959a3b933b
Andre Bellmann <andre.bellmann@huk-coburg.de>
2020-12-18 16:02:39 +0100

    circlebutton

---
+++ src/components/CircleButton.svelte
@@ -0,0 +1,28 @@
+<style>
+  .circlebutton {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--bg-100);
+    cursor: pointer;
+    border: none;
+    appearance: none;
+    color: inherit;
+    transition: border-color 0.2s ease-out;
+    border: 2px solid var(--bg-100);
+  }
+  .circlebutton:hover {
+    border-color: var(--bg-50);
+  }
+  .circlebutton:active {
+    background-color: var(--bg-200);
+  }
+</style>
+
+<button class="circlebutton">
+  <slot />
+</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -135,6 +135,7 @@
   import Timetravel from '../components/Timetravel.svelte';
   import { sortCitys } from '../utils/sortCitys';
   import Message from '../components/Message.svelte';
+  import CircleButton from '../components/CircleButton.svelte';
 
   export let data;
 
@@ -150,6 +151,11 @@
       germannew = $async_data.germannew;
       bavarianew = $async_data.bavarianew;
     }
+  }
+
+  // Handle click on circle button
+  function handleCircleButtonClick() {
+    console.log('Clicki');
   }
 </script>
 
@@ -196,7 +202,22 @@
     </li>
   </ul>
 </nav>
-
+<CircleButton on:click="{() => handleCircleButtonClick()}">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    focusable="false"
+    role="img"
+    width="1em"
+    height="1em"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  ><path
+      d="M4 2v6H2V2h2M2 22v-6h2v6H2m3-10c0 1.11-.89 2-2 2a2 2 0 1 1 2-2m11-8c4.42 0 8 3.58 8 8s-3.58 8-8 8c-3.6 0-6.64-2.38-7.65-5.65L6 12l2.35-2.35C9.36 6.38 12.4 4 16 4m-1 9l4.53 2.79l.8-1.29l-3.83-2.3V7H15v6z"
+      fill="currentColor"
+    ></path></svg>
+</CircleButton>
 <Timetravel />
 <div class="hints">
   <Message until="2021-01-05T00:00:00Z">
