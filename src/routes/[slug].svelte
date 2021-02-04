26112bc0b02885df0f2aef92e082894ca8fcb941
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-02-04 16:53:00 +0100

    h1 change

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -260,7 +260,7 @@
 </script>
 
 <svelte:head>
-  <title>Corona-Ampel für {city.name}: ⚠️ Alle aktuellen Zahlen</title>
+  <title>Corona-Ampel für {city.name}: ⚠️ Alle aktuellen Covid-Zahlen und Inzidenz</title>
   <meta
     content="{`⚠️ Alle aktuellen Zahlen für ${city.name}: ⚠️ ${city.newCases} neue Fälle - ⚠️ Aktuelle Inzidenz: {city.incidence}`}"
     name="description"
