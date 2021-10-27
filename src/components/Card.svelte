5029eb60745f6f5a6a99de0182e4af3d7d086f67
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-10-27 10:53:34 +0200

    more contrast in card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -127,7 +127,7 @@
 
       if (!!data.allIncidences) {
         // ctx.fillStyle = data.incidence >= 100 ? 'rgba(255,100,100,0.1)' : 'rgba(0,0,0,0.1)';
-        ctx.fillStyle = 'rgba(0,0,0,0.1)';
+        ctx.fillStyle = 'rgba(0,0,0,0.14)';
 
         // draw the area
         ctx.beginPath();
@@ -140,7 +140,7 @@
         // Last point
         ctx.lineTo(610, height);
         // ctx.strokeStyle = data.incidence >= 100 ? 'rgba(255,100,100,0.2)' : 'rgba(0,0,0,0.2)';
-        ctx.strokeStyle = 'rgba(0,0,0,0.2)';
+        ctx.strokeStyle = 'rgba(0,0,0,0.3)';
         ctx.lineWidth = 1;
         ctx.stroke();
         ctx.fill();
