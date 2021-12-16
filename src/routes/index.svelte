a08bc063f68121f6145a4fd73c774713c6ae239d
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-12-16 17:17:46 +0100

    spritze

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,7 @@
 <style>
+  .homepage {
+    cursor: url('../cursor.png'), default;
+  }
   a {
     text-decoration: none;
   }
@@ -154,118 +157,121 @@
     content="🚦 Aktuelle 7 Tage-Inzidenz pro 100.000 Einwohner für Coburg, Bamberg, Kronach, Schweinfurt, Lichtenfels, Haßberge, Hildburghausen und Sonneberg !  ⚠️ Alle aktuellen Zahlen ⚠️ Alle aktuellen Fälle"
   />
 </svelte:head>
-<center>
-  <Message until="2021-12-15T23:00:00Z">
-    <!-- +1 Stunde -->
-    <a class="list-tile" href="https://corona-ampel.app/?coburg">
-      <div class="logo">
-        <img
-          src="https://www.corona-ampel.app/favicons/maskable_icon_x128.png"
-          width="24"
-          height="24"
-          alt="Ampel-Icon"
-          style=" height: auto;
+
+<div class="homepage">
+  <center>
+    <Message until="2021-12-15T23:00:00Z">
+      <!-- +1 Stunde -->
+      <a class="list-tile" href="https://corona-ampel.app/?coburg">
+        <div class="logo">
+          <img
+            src="https://www.corona-ampel.app/favicons/maskable_icon_x128.png"
+            width="24"
+            height="24"
+            alt="Ampel-Icon"
+            style=" height: auto;
   max-width: 100%;"
-        />
-      </div>
-      <p class="details">
-        <small> <strong>Zur App mit ALLEN Landkreisen&nbsp;&rsaquo;</strong> </small>
-      </p>
-    </a>
-  </Message>
-</center>
+          />
+        </div>
+        <p class="details">
+          <small> <strong>Zur App mit ALLEN Landkreisen&nbsp;&rsaquo;</strong> </small>
+        </p>
+      </a>
+    </Message>
+  </center>
 
-<center>
-  <Message until="2021-09-09T17:00:00Z">
-    <!-- +1 Stunde -->
-    <small>
-      <span> 💥 <strong>ACHTUNG!</strong> Das RKI aktualisiert aktuell noch ihre Daten ... </span>
-    </small>
-  </Message>
-</center>
+  <center>
+    <Message until="2021-09-09T17:00:00Z">
+      <!-- +1 Stunde -->
+      <small>
+        <span> 💥 <strong>ACHTUNG!</strong> Das RKI aktualisiert aktuell noch ihre Daten ... </span>
+      </small>
+    </Message>
+  </center>
 
-<Snow />
+  <Snow />
 
-<div style="margin-top:10px;">
-  <ul class="cols2">
-    <li>
-      <CardNarrow bind:data="{germanycard}" />
-    </li>
-    <li>
-      <CardNarrow bind:data="{bavariacard}" />
-    </li>
-  </ul>
-</div>
-
-{#if !!hospitalization}
-  <h2>Hospitalisierung in Bayern</h2>
-  <Hospitalization bind:data="{hospitalization[0]}" />
-  <h2>7 Tage Inzidenz</h2>
-{/if}
-
-<nav>
-  <ul id="hp-cardlist">
-    {#each sortedCitys as city}
+  <div style="margin-top:10px;">
+    <ul class="cols2">
       <li>
-        <a rel="prefetch" href="{city.slug}/" title="{`Zu ${city.district} ${city.name} ›`}">
-          <Card bind:data="{city}" />
-        </a>
+        <CardNarrow bind:data="{germanycard}" />
       </li>
-    {/each}
-  </ul>
-</nav>
-<div class="social">
-  <Social />
-</div>
-<br />
-<center>
-  <Message until="2024-09-09T17:00:00Z" left>
-    <!-- +1 Stunde -->
-    <small>
-      <span><strong>Hinweis</strong>: Alle Angaben sind ohne Gewähr. Dies ist nicht die offizielle
-        Corona-Ampel. Farben können eventuell abweichen und sind nur zur Orientierung gedacht.</span>
-    </small>
-  </Message>
-  <Message until="2024-09-09T17:00:00Z" left>
-    <!-- +1 Stunde -->
-    <small>
-      <span><strong>Hinweis zur Hospitalisierung</strong>: Die wirklichen Zahlen zur
-        Hospitalisierung können nicht zu 100% korrekt angegeben werden, da es über Tage und Wochen
-        hinweg noch Nachmeldungen gibt. Ich kann hier leider nur die offiziellen Zahlen des LGLs
-        angeben, nach der sich unsere Regierung in Bayern orientiert.
-        <a
-          href="https://www.lgl.bayern.de/gesundheit/infektionsschutz/infektionskrankheiten_a_z/coronavirus/karte_coronavirus/index.htm#wKennzahlen"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <small>Quelle: LGL</small>
-        </a></span>
-    </small>
-  </Message>
-</center>
+      <li>
+        <CardNarrow bind:data="{bavariacard}" />
+      </li>
+    </ul>
+  </div>
 
-<Timetravel />
-<div class="hints">
-  <Message until="2021-01-31T00:00:00Z">
-    <small>
-      <svg
-        class="danger-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        aria-hidden="true"
-        focusable="false"
-        role="img"
-        width="1.5em"
-        height="1.5em"
-        preserveAspectRatio="xMidYMid meet"
-        viewBox="0 0 24 24"
-        style="transform: translate3d(0, -2px, 0);
+  {#if !!hospitalization}
+    <h2>Hospitalisierung in Bayern</h2>
+    <Hospitalization bind:data="{hospitalization[0]}" />
+    <h2>7 Tage Inzidenz</h2>
+  {/if}
+
+  <nav>
+    <ul id="hp-cardlist">
+      {#each sortedCitys as city}
+        <li>
+          <a rel="prefetch" href="{city.slug}/" title="{`Zu ${city.district} ${city.name} ›`}">
+            <Card bind:data="{city}" />
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+  <div class="social">
+    <Social />
+  </div>
+  <br />
+  <center>
+    <Message until="2024-09-09T17:00:00Z" left>
+      <!-- +1 Stunde -->
+      <small>
+        <span><strong>Hinweis</strong>: Alle Angaben sind ohne Gewähr. Dies ist nicht die offizielle
+          Corona-Ampel. Farben können eventuell abweichen und sind nur zur Orientierung gedacht.</span>
+      </small>
+    </Message>
+    <Message until="2024-09-09T17:00:00Z" left>
+      <!-- +1 Stunde -->
+      <small>
+        <span><strong>Hinweis zur Hospitalisierung</strong>: Die wirklichen Zahlen zur
+          Hospitalisierung können nicht zu 100% korrekt angegeben werden, da es über Tage und Wochen
+          hinweg noch Nachmeldungen gibt. Ich kann hier leider nur die offiziellen Zahlen des LGLs
+          angeben, nach der sich unsere Regierung in Bayern orientiert.
+          <a
+            href="https://www.lgl.bayern.de/gesundheit/infektionsschutz/infektionskrankheiten_a_z/coronavirus/karte_coronavirus/index.htm#wKennzahlen"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <small>Quelle: LGL</small>
+          </a></span>
+      </small>
+    </Message>
+  </center>
+
+  <Timetravel />
+  <div class="hints">
+    <Message until="2021-01-31T00:00:00Z">
+      <small>
+        <svg
+          class="danger-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          focusable="false"
+          role="img"
+          width="1.5em"
+          height="1.5em"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 24 24"
+          style="transform: translate3d(0, -2px, 0);
   vertical-align: middle;"
-      ><path
-          d="M14.8 4.613l6.701 11.161c.963 1.603.49 3.712-1.057 4.71a3.213 3.213 0 0 1-1.743.516H5.298C3.477 21 2 19.47 2 17.581c0-.639.173-1.264.498-1.807L9.2 4.613c.962-1.603 2.996-2.094 4.543-1.096c.428.276.79.651 1.057 1.096zm-2.22.839a1.077 1.077 0 0 0-1.514.365L4.365 16.98a1.17 1.17 0 0 0-.166.602c0 .63.492 1.14 1.1 1.14H18.7c.206 0 .407-.06.581-.172a1.164 1.164 0 0 0 .353-1.57L12.933 5.817a1.12 1.12 0 0 0-.352-.365zM12 17a1 1 0 1 1 0-2a1 1 0 0 1 0 2zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"
-          fill="currentColor"
-        ></path></svg>
-      <span> Ab 200 gelten weitere Maßnahmen in Bayern! </span>
-    </small>
-  </Message>
+        ><path
+            d="M14.8 4.613l6.701 11.161c.963 1.603.49 3.712-1.057 4.71a3.213 3.213 0 0 1-1.743.516H5.298C3.477 21 2 19.47 2 17.581c0-.639.173-1.264.498-1.807L9.2 4.613c.962-1.603 2.996-2.094 4.543-1.096c.428.276.79.651 1.057 1.096zm-2.22.839a1.077 1.077 0 0 0-1.514.365L4.365 16.98a1.17 1.17 0 0 0-.166.602c0 .63.492 1.14 1.1 1.14H18.7c.206 0 .407-.06.581-.172a1.164 1.164 0 0 0 .353-1.57L12.933 5.817a1.12 1.12 0 0 0-.352-.365zM12 17a1 1 0 1 1 0-2a1 1 0 0 1 0 2zm0-9a1 1 0 0 1 1 1v4a1 1 0 0 1-2 0V9a1 1 0 0 1 1-1z"
+            fill="currentColor"
+          ></path></svg>
+        <span> Ab 200 gelten weitere Maßnahmen in Bayern! </span>
+      </small>
+    </Message>
+  </div>
 </div>
